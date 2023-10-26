a75b69b5c8ba9b84b185fb02e03550633824c543
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-26 13:35:29 -0700

    set up mongodb cloud connection

--- user-services.js
+++ user-services.js
@@ -1,3 +1,27 @@
 import "dotenv"
+import mongoose from "mongoose";
 
 const dotenv = require("dotenv")
+dotenv.config();
+
+
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.MONGO_USER +
+      ":" +
+      process.env.MONGO_PWD +
+      "@" +
+      process.env.MONGO_CLUSTER +
+      "/" +
+      process.env.MONGO_DB +
+      "?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true, //useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
+  .catch((error) => console.log(error));
