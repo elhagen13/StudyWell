7dc4f2d558f9c24a677653a27dece217876fc138
Ella Hagen <elhagen@calpoly.edu>
2023-10-30 08:27:29 -0700

    added task bar and add task button

---
+++ packages/studywell-frontend/src/Task.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+function Task(props) {
+  
+  const [task, setTask] = useState(
+     {
+        task: "",
+     }
+  );
+
+  function submitTask() {
+    props.handleSubmit(task);
+    setTask({task: ''});
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setTask(
+      {task: value}
+    );
+    
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        task = "task"
+        value={task.task}
+        onChange={handleChange} />
+      <input
+        type = "button"
+        value = "Submit"
+        onClick = {submitTask}/>
+      </form>
+    );
+}
+export default Task;
