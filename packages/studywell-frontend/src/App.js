16db5625e620c5b213c919260bef3cdb53fb20ff
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-11-12 18:06:20 -0800

    added postUser and updateList in app.js

--- packages/studywell-frontend/src/App.js
+++ packages/studywell-frontend/src/App.js
@@ -1,37 +1,74 @@
-import './App.css';
-import './Timer.css';
-import React, { useState, useEffect } from 'react';
-import Task from './Task'
-import TaskBar from './TaskBar'
-import Timer from './Timer';
-import StartButton from './StartButton';
+import "./App.css";
+import "./Timer.css";
+import React, { useState, useEffect } from "react";
+import Task from "./Task";
+import TaskBar from "./TaskBar";
+import Timer from "./Timer";
+import StartButton from "./StartButton";
 
 function App() {
-  
   const [tasks, setTasks] = useState([]);
 
-  function removeTask(index){
+  function removeTask(index) {
     const updated = tasks.filter((task, i) => {
-      return i !== index
+      return i !== index;
     });
     setTasks(updated);
   }
+  //   ******* Please confirm if this can be deleted or not! *****
 
-  function updateList(task) {
-    console.log(task);
-    setTasks([...tasks, task]);
-    console.log(tasks)
+  // function updateList(task) {
+  //   console.log(task);
+  //   setTasks([...tasks, task]);
+  //   console.log(tasks)
+  // }
+  function postUser(task) {
+    const promise = fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    return promise;
   }
 
+  function updateList(task) {
+    postUser(task)
+      .then((res) => {
+        console.log(res);
+        return res.status === 200 ? res.json() : undefined;
+      })
+      .then((json) => {
+        if (json) setTasks([...tasks, json]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  function fetchTasks() {
+    const promise = fetch("http://localhost:8000/tasks");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchTasks()
+      .then((res) => res.json())
+      .then((json) => setTasks(json["task_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
-    <div className = "App">
+    <div className="App">
       <div className="container">
-        <Timer time = {25}/>
+        <Timer time={25} />
       </div>
       <div className="task_bar">
-        <TaskBar tasksData = {tasks}
-        removeTask={removeTask}/>
-        <Task handleSubmit = {updateList}/>
+        <TaskBar tasksData={tasks} removeTask={removeTask} />
+        <Task handleSubmit={updateList} />
       </div>
     </div>
   );
--- studywell-backend/backend.js
+++ studywell-backend/backend.js
@@ -2,54 +2,52 @@ import express from "express";
 import cors from "cors";
 import taskModel from "./task-functions.js";
 
-
 const app = express();
 const port = 8000;
-
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});  
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 //generates random ID
 const generateID = () => {
-    const id = Math.floor(100000 + Math.random() * 900000);
-    const newID = id.toString();
-    var count = 0;
-    return newID;
-}
+  const id = Math.floor(100000 + Math.random() * 900000);
+  const newID = id.toString();
+  var count = 0;
+  return newID;
+};
 
-app.get('/tasks', (req,res) => {
-    taskModel.getTasks(req.description)
-        .then((result) => {
-            res.send({task_list: result})
-        })
+app.get("/tasks", (req, res) => {
+  taskModel.getTasks(req.description).then((result) => {
+    res.send({ task_list: result });
+  });
 });
 
-app.post('/tasks', (req,res) => {
-    const taskToAdd = req.body;
-    taskToAdd.id = generateID();
-    taskModel.addTasks(taskToAdd)
-        .then((result) => {
-            res.status(201).send(taskToAdd);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send(error);
-        });
+app.post("/tasks", (req, res) => {
+  const taskToAdd = req.body;
+  taskToAdd.id = generateID();
+  taskModel
+    .addTasks(taskToAdd)
+    .then((result) => {
+      res.status(201).send(taskToAdd);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send(error);
+    });
 });
 async function deleteUserById(id) {
-    try {
-      if (await taskModel.deleteUser(id)) return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+  try {
+    if (await taskModel.deleteUser(id)) return true;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
+}
 app.delete("/tasks/:id", (req, res) => {
-    const id = req.params["id"];
-    deleteUserById(id);
+  const id = req.params["id"];
+  deleteUserById(id);
 });
