2d784105a8a53b0cc9b901b8ae9ccb84237c7daa
Ella Hagen <elhagen@calpoly.edu>
2023-11-19 07:10:20 -0800

    changed some color box problems

--- packages/studywell-frontend/src/pages/LongBreak.js
+++ packages/studywell-frontend/src/pages/LongBreak.js
@@ -3,6 +3,8 @@ import "../components/timer/Timer.css";
 import React from "react";
 import Timer from "../components/timer/Timer";
 import "./Page.css";
+import ColorBox from "../components/colorbox/ColorBox";
+
 
 function LongBreak() {
   return (
@@ -13,6 +15,7 @@ function LongBreak() {
       <div className="break_text">
         <p>Stretch and grab a snack!</p>
       </div>
+      <ColorBox />
     </div>
   );
 }
