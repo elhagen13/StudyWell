bd3aee55760216b45afef7674b8e91c1db623f99
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2023-11-27 09:48:19 -0800

    moved Timer.css

--- packages/studywell-frontend/src/pages/MainScreen.js
+++ packages/studywell-frontend/src/pages/MainScreen.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import "../components/timer/Timer.css";
+import "../Timer.css";
 import React, { useState } from "react";
 import Task from "../components/taskbar/Task";
 import TaskBar from "../components/taskbar/TaskBar";
