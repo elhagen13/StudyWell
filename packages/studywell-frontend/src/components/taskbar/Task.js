523457e10f1436f4f8c52d9dc6bbc7e6aab9b721
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2023-11-27 09:16:25 -0800

    added pomodoros to tasks

--- packages/studywell-frontend/src/components/taskbar/Task.js
+++ packages/studywell-frontend/src/components/taskbar/Task.js
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
+import { set } from "mongoose";
 
 function Task(props) {
   const [task, setTask] = useState({
     task: "type here...",
+    pomodoros: '',
   });
 
   function submitTask() {
     props.handleSubmit(task);
-    setTask({ task: "type here..." });
+    setTask({ ...task, task: "type here...", pomodoros: '' });
   }
 
-  function handleChange(event) {
+  function handleTaskChange(event) {
     const { value } = event.target;
-    setTask({ task: value });
+    setTask({ ...task, task: value });
+  }
+
+  function handlePomodoroChange(event) {
+    const {value} = event.target;
+    setTask({...task, pomodoros: Number(value) });
   }
 
   function handleInputClick(event) {
-    setTask({ task: "" });
+    setTask({ ...task, task: "" });
   }
 
   const placeholderStyle = {
@@ -24,18 +31,29 @@ function Task(props) {
   };
 
   return (
-    <form className="submit_row">
-      <input
-        type="text"
-        task="task"
-        class="task_form"
-        style={task.task === "type here..." ? placeholderStyle : {}}
-        value={task.task}
-        onChange={handleChange}
-        onClick={handleInputClick}
-      />
-      <input type="button" class="submit_task" value="+" onClick={submitTask} />
-    </form>
+    <div>
+      <form className="submit_row">
+        <input
+          type="text"
+          task="task"
+          class="task_form"
+          style={task.task === "type here..." ? placeholderStyle : {}}
+          value={task.task}
+          onChange={handleTaskChange}
+          onClick={handleInputClick}
+        />
+        <input
+          type="number"
+          // style={task.pomodoros === 0 ? placeholderStyle : {}}
+          placeholder="# of pomodoros"
+          onChange={handlePomodoroChange}
+          value={task.pomodoros}
+        />
+        <input type="button" class="submit_task" value="+" onClick={submitTask} />
+      </form>
+    </div>
+
+    
   );
 }
 export default Task;
