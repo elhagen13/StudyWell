d8c2867e978090393c145662f54aab9dfc4883b6
Brandon Eng <ebdon101@gmail.com>
2023-11-27 10:00:25 -0800

    Fixed prettier stylings and timer warnings

--- packages/studywell-frontend/src/components/timer/Timer.js
+++ packages/studywell-frontend/src/components/timer/Timer.js
@@ -1,9 +1,13 @@
-import React, {useEffect, useState, useRef, useMemo, useCallback} from "react";
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+} from "react";
 import StartButton from "./StartButton";
 import bluemingsound from "../../sounds/blueming-alarm.mp3"; // Update the path to your alarm sound file
 import { useNavigate } from "react-router-dom"; // Import useNavigate
-
-
 
 function Timer(props) {
   const [minutes, setMinutes] = useState(props.time);
@@ -11,59 +15,48 @@ function Timer(props) {
   const [timerOn, setTimerOn] = useState(false);
   const [timerDone, setTimerDone] = useState(false);
   const timerRef = useRef(null);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [breakCount, setBreakCount] = useState(0);
 
+  const audio = useMemo(() => new Audio(bluemingsound), []);
 
-  const audio = useMemo(() => new Audio(bluemingsound), []); 
-  
   const endTimer = useCallback(() => {
-  
     setTimerDone(true);
     audio.play();
-    setBreakCount(count => count + 1);
+    setBreakCount((count) => count + 1);
     console.log(breakCount);
-    if (breakCount % 3 === 0){
+    if (breakCount % 3 === 0) {
       navigate("/long");
+    } else {
+      navigate("/short");
     }
-    else{
-      navigate("/short")
-    }
+  }, [audio, breakCount, navigate]);
 
-  },[audio, breakCount, navigate]);
-
-  useEffect(()=>{
-
+  useEffect(() => {
     if (timerOn) {
-
-    timerRef.current = setInterval(() => {
-
-      if(seconds > 0){
-      setSeconds(seconds - 1);
-      }
-      else if(seconds === 0 && minutes > 0){
-        setMinutes(minutes - 1);
-        setSeconds(3);
-      }
-      else{
-        clearInterval(timerRef.current);
-        setTimerOn(false);
-        endTimer();
-        audio.play();
-
-      }
-    }, 1000)}
-    return ()=>clearInterval(timerRef.current);
+      timerRef.current = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        } else if (seconds === 0 && minutes > 0) {
+          setMinutes(minutes - 1);
+          setSeconds(3);
+        } else {
+          clearInterval(timerRef.current);
+          setTimerOn(false);
+          endTimer();
+          audio.play();
+        }
+      }, 1000);
+    }
+    return () => clearInterval(timerRef.current);
   }, [timerOn, minutes, seconds, audio, endTimer]);
 
   function startTimer() {
     setTimerOn(true);
-  };
-  function pauseTimer(){
+  }
+  function pauseTimer() {
     setTimerOn(false);
   }
-
-
 
   useEffect(() => {
     if (timerDone) {
@@ -73,27 +66,24 @@ function Timer(props) {
 
   return (
     <div className="container">
-
-
-    { timerDone ? (
-     
-      <div className="timer-container">
-        <h1>Finished</h1>
-      </div>
-
-
-    ) :
-
-    <div className="timer-container">
-      <h1>{minutes<10? "0"+minutes: minutes}:{seconds<10? "0"+seconds: seconds}</h1>
-      <StartButton
-        timerOn={timerOn}
-        startTimer={startTimer}
-        pauseTimer={pauseTimer}
-      />
+      {timerDone ? (
+        <div className="timer-container">
+          <h1>Finished</h1>
+        </div>
+      ) : (
+        <div className="timer-container">
+          <h1>
+            {minutes < 10 ? "0" + minutes : minutes}:
+            {seconds < 10 ? "0" + seconds : seconds}
+          </h1>
+          <StartButton
+            timerOn={timerOn}
+            startTimer={startTimer}
+            pauseTimer={pauseTimer}
+          />
+        </div>
+      )}
     </div>
-    }
-  </div>
   );
 }
 
