974770ced7b2846dc9bfb28c0359b438d521d453
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-11-27 09:38:55 -0800

    Resolve merge conflicts with post route

--- studywell-backend/backend.js
+++ studywell-backend/backend.js
@@ -5,37 +5,26 @@ import taskModel from "./task-functions.js";
 const app = express();
 const port = 8000;
 
-<<<<<<< HEAD
 app.use(cors());
 app.use(express.json());
 
 /* eslint-disable-next-line no-undef*/
 app.listen(process.env.PORT || port, () => {
   console.log("REST API is listening.");
-=======
-app.use(express.json());
-app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
->>>>>>> backend
-});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
 
 //generates random ID
 const generateID = () => {
   const id = Math.floor(100000 + Math.random() * 900000);
   const newID = id.toString();
-<<<<<<< HEAD
-  return newID;
-};
-
-/* eslint-disable no-unused-vars */
-=======
   var count = 0;
   return newID;
 };
 
->>>>>>> backend
 app.get("/tasks", (req, res) => {
   taskModel.getTasks(req.description).then((result) => {
     res.send({ task_list: result });
@@ -44,18 +33,11 @@ app.get("/tasks", (req, res) => {
 
 app.post("/tasks", (req, res) => {
   const taskToAdd = req.body;
-<<<<<<< HEAD
-  taskToAdd.id = generateID();
-  taskModel
-    .addTasks(taskToAdd)
-    .then((res) => {
-=======
   console.log(taskToAdd);
   taskToAdd.id = generateID();
   taskModel
     .addTasks(taskToAdd)
     .then((result) => {
->>>>>>> backend
       res.status(201).send(taskToAdd);
     })
     .catch((error) => {
@@ -63,6 +45,7 @@ app.post("/tasks", (req, res) => {
       res.status(500).send(error);
     });
 });
+
 async function deleteUserById(id) {
   try {
     if (await taskModel.deleteUser(id)) return true;
@@ -84,5 +67,5 @@ app.delete("/tasks/:id", async (req, res) => {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
->>>>>>> backend
 });
+
